87b6b6cc67b375bbdfe3f973d79cd7f158d0ee7e
mahabub-bd <palashmahabub@gmail.com>
2024-02-28 15:47:16 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="mt-5 font-bold text-rose-600 text-center text-3xl">
+          React Infinite Scrolling
+        </h1>
+        <ProductList />
+      </div>
+    </>
+  );
+}
+
+export default App;
